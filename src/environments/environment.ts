10a9b9becf220b16a010b3ce590a7b90f766b70f
Gionata Garavani <gionatagaravani@libero.it>
2024-01-18 21:57:04 +0100

    firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+        "projectId" : "angular-17-crud",
+        "appId" : "1:364034409475:web:795514f6c6898b70e87a36",
+        "databaseURL" : "https://angular-17-crud-default-rtdb.firebaseio.com",
+        "storageBucket" : "angular-17-crud.appspot.com",
+        "apiKey" : "AIzaSyA-QcJMewXvlgnRiZExhIYstmxCDuYPexk",
+        "authDomain" : "angular-17-crud.firebaseapp.com",
+        "messagingSenderId" : "364034409475"
+    },
+    production: false
+}
